db3cf6774be830e864d05b25c9cede25527e2c90
davidrfreeman <david.freeman12@gmail.com>
2018-08-12 14:53:42 -0600

    Add vue-bootstrap to project

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,7 +3,7 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: 'portfolio-page',
+    title: 'David Freeman',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
@@ -17,6 +17,14 @@ module.exports = {
   ** Customize the progress bar color
   */
   loading: { color: '#3B8070' },
+
+  modules: [
+    "bootstrap-vue/nuxt",
+
+    // Or if you have custom bootstrap CSS...
+    ["bootstrap-vue/nuxt", { css: false }]
+  ],
+  
   /*
   ** Build configuration
   */

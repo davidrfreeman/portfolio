c09bfcb229052c743c4f3eb2cdc1f3fcd6dabd8d
davidrfreeman <david.freeman12@gmail.com>
2018-08-12 20:53:54 -0600

    Fix linting errors

--- pages/index.vue
+++ pages/index.vue
@@ -14,7 +14,7 @@
 </template>
 
 <script>
-import AppLogo from '~/components/AppLogo.vue'
+import AppLogo from "~/components/AppLogo.vue"
 
 export default {
   components: {
@@ -33,7 +33,8 @@ export default {
 }
 
 .title {
-  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
+  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont,
+    "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
   display: block;
   font-weight: 300;
   font-size: 100px;
@@ -53,4 +54,3 @@ export default {
   padding-top: 15px;
 }
 </style>
-

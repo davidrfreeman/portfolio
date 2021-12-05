d2b12e19d242ad87cf6a2a52227a1e3ecb7e1879
David Freeman <13970707+davidrfreeman@users.noreply.github.com>
2021-12-05 15:55:20 -0700

    catch up commit

--- pages/index.vue
+++ pages/index.vue
@@ -1,51 +1,21 @@
 <template>
-  <b-container fluid>
+  <b-container>
     <b-row>
-      <b-col>
-        <h1 class="title">
-          David Freeman
-        </h1>
+      <b-col cols="12" lg="8">
+        <p>My name is David Freeman and I am a developer. I am currently pursuing my Masters at Regis University in Software Engineering. I began learning web development in 2015 after the local steel mill I was working at was hit by a downturn in the economy.</p>
+        <p>I had to decide what my next move would be. I had a bachelors from Colorado State University-Pueblo in Business majoring in Economics (possibly should have seen the lay-offs coming), and finding a new job in my area proved difficult. A friend of mine had just started his own business making websites and encouraged me to look at getting into this field. I used free online tutorials from Codecademy and FreeCodeCamp to refresh my dusty and outdated knowledge of HTML and CSS, and to also learn JavaScript. I then bought a couple of courses from Udemy, and also found Coursera and EdX, taking courses in programming.</p>
+        <p>I returned to my position at the mill in 2017 and a month later took a position on the Sales Team as a Sales Analyst. I used spreadsheets a fair amount in college, but never to the degree that I did in this role. I built a number of dashboards to report on the group's performance. I assited with Inside Sales, entering orders using Oracle 11i and scheduling shipments, I also represented my group on a weekly collections call, which meant I would send out emails weekly to customers with past due balances to get updates on their payment plans.</p>
+        <p>I worked on a project for a few months as a frontend developer and gained experience with Vue js. I enjoyed working with Vue so when I set out to build my own site this is where I started. I then learned about Nuxt and began experimenting with the Starter Template installed from the Vue CLI. In an effort to keep the cost of this site down I am using GitHub (GH) Pages. I wanted to do this with a static site for which Nuxt is used. Nuxt's documentation is great and was able to get this up and running shortly, I also followed the instructions to automated the build and deployment step with Travis CI. Now each time I commit to the master branch the changes are deployed to my GH Pages branch for this site.</p>
+        <p>My time spent working on side projects paid off though. I was talking with someone who worked in IT about my side projects when they mentioned IT had an open position I should apply for. I got an interview and was hired as a Systems Engineer I working on Oracle applications.</p>
+        <p>When I joined the IT team I began learning about Oracle SQL and PL/SQL. To do this I was assigned maintenance tasks to work on existing Oracle Forms and Reports. Doing this allowed for me to learn about the Forms and Reports IDEs and also how most of our apps functioned.</p>
       </b-col>
     </b-row>
-    <about />
   </b-container>
 </template>
 
 <script>
 export default {}
 </script>
-
-<style>
-.container {
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont,
-    "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
-</style>
 
 <script>
 import about from "@/pages/about"
